fea42b77d4c20589a9ae0039cda69fba4de940df
Demid <myemail@foobar.com>
2025-09-29 21:29:17 +0300

    feat(cipher): impl Cipher class performing Grain-128a
    This commit adds implementation of Cipher class which provides methods
    for encryption and decryption of data with Grain-128a

--- core/cipher/cipher.cpp
+++ core/cipher/cipher.cpp
@@ -2,6 +2,71 @@
 
 using namespace grain;
 
+
+Cipher::Cipher(const Key& key, const InitVector& iv) : nfsr{key}, lfsr{iv}
+{
+    init_auth_registers(); 
+    warmup();
+}
+
+
+void Cipher::init_auth_registers()
+{
+    auto init_data = [this](uint8_t* data)
+    {
+        for (int i = 0; i < 32; ++i)
+        {
+            data[i] = keystream_iter();
+        }
+    };
+
+    uint8_t accum_init[32];
+    uint8_t shift_reg_init[32];
+
+    init_data(accum_init);
+    init_data(shift_reg_init);
+
+    ar.init(accum_init, shift_reg_init);
+}
+
+
+void Cipher::warmup(int iterations = 256)
+{
+    for (int i = 0; i < iterations; ++i)
+    {
+        keystream_iter(true);
+    }
+}
+
+
+uint8_t Cipher::keystream_iter(bool warmup = false)
+{
+    uint8_t pf = preoutput_function();
+    uint8_t lfsr_new_bit = lfsr.generate_next_bit();
+    uint8_t nfsr_new_bit = nfsr.generate_next_bit(lfsr_new_bit);
+
+    lfsr.shift_left_by_one(lfsr_new_bit ^ (warmup ? pf : 0));
+    nfsr.shift_left_by_one(nfsr_new_bit ^ (warmup ? pf : 0));
+
+    return pf;
+}
+
+
+uint8_t Cipher::preoutput_function()
+{
+    Nfsr128& b = nfsr;
+    Lfsr128& s = lfsr;
+
+    auto h = [b, s]()
+    {
+        return (b[12] & s[8]) ^ (s[13] & s[20]) ^ (b[95] & s[42]) ^
+                (s[60] & s[79]) ^ (b[12] & b[95] & s[94]);
+    };
+
+    return h() ^ s[93] ^ b[2] ^ b[15] ^ b[36] ^ b[45] ^ b[64] ^ b[73] ^ b[89];
+}
+
+
 void Cipher::encrypt(uint8_t* data, size_t length, uint8_t* output) {
     for (size_t i = 0; i < length; ++i) {
         uint8_t keystream_byte = 0;
@@ -17,6 +82,7 @@ void Cipher::encrypt(uint8_t* data, size_t length, uint8_t* output) {
         output[i] = data[i] ^ keystream_byte;
     }
 }
+
 
 void Cipher::decrypt(uint8_t* data, size_t length, uint8_t* output) {
     // Decryption is the same as encryption in Grain-128a
--- core/cipher/cipher.h
+++ core/cipher/cipher.h
@@ -1,25 +1,52 @@
 #ifndef CIPHER_H
 #define CIPHER_H
 
-#include "shift_registers.h"
+#include "nfsr_128.h"
+#include "lfsr_128.h"
+#include "auth_registers.h"
+
 #include <cstdint>
 #include <cstddef>
 
-namespace grain {
-    class Cipher {
+
+namespace grain
+{
+
+/// @brief Implementation of Grain-128a cipher
+class Cipher {
     public:
+        /// @brief Initialize cipher instance
+        /// @param key initialization key for NFSR
+        /// @param iv initialization vector for LFSR
+        Cipher(const Key& key, const InitVector& iv);
+
+        /// @brief Encrypt data stored under `data` pointer, write encrypted to `output`
+        /// @param data pointer to data to be encrypted
+        /// @param length length of data to be encrypted
+        /// @param output pointer to memory to store output
         void encrypt(uint8_t *data, size_t length, uint8_t *output);
+        
+        /// @brief Decrypt data stored under `data` pointer, write decrypted to `output`
+        /// @param data pointer to data to be decrypted
+        /// @param length length of data to be decrypted
+        /// @param output pointer to memory to store output
         void decrypt(uint8_t *data, size_t length, uint8_t *output);
 
     private:
-        NFSR nfsr;
-        LFSR lfsr;
+        Nfsr128 nfsr;
+        Lfsr128 lfsr;
+        AuthRegisters ar;
 
-        void init();
-        void warmup();
+        /// @brief Clock a number of iterations without using keystream results
+        /// @param iterations number of iterations to clock
+        void warmup(int iterations = 256);
+
+        /// @brief Initialize authentication registers by first 64 keystream cycles
+        void init_auth_registers();
         uint8_t keystream_iter(bool warmup = false);
         uint8_t preoutput_function();
-    };
-}
+};
+
+} // namespace grain
 
 #endif // CIPHER_H
